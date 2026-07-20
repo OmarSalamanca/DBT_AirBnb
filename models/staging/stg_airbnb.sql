14b7525534b6e1dc7f0dc494b65e3610a1dcf72d
ossossoss2345@gmail.com <ossossoss2345@gmail.com>
2026-07-20 21:31:46 +0000

    Clean text columns by trimming whitespace

--- models/staging/stg_airbnb.sql
+++ models/staging/stg_airbnb.sql
@@ -1,19 +1,19 @@
 select
 
     ID as listing_id,
-    NAME as listing_name,
+    trim(NAME) as listing_name,
     HOST_ID as host_id,
-    HOST_NAME as host_name,
+    trim(HOST_NAME) as host_name,
     HOST_IDENTITY_VERIFIED as host_identity_verified,
-    NEIGHBOURHOOD_GROUP as neighbourhood_group,
-    NEIGHBOURHOOD as neighbourhood,
+    trim(NEIGHBOURHOOD_GROUP) as neighbourhood_group,
+    trim(NEIGHBOURHOOD) as neighbourhood,
     LAT as latitude,
     LONG as longitude,
-    COUNTRY as country,
-    COUNTRY_CODE as country_code,
+    trim(COUNTRY) as country,
+    trim(COUNTRY_CODE) as country_code,
     INSTANT_BOOKABLE as instant_bookable,
     CANCELLATION_POLICY as cancellation_policy,
-    ROOM_TYPE as room_type,
+    trim(ROOM_TYPE) as room_type,
     CONSTRUCTION_YEAR as construction_year,
     MINIMUM_NIGHTS as minimum_nights,
     NUMBER_OF_REVIEWS as number_of_reviews,
@@ -22,7 +22,7 @@ select
     REVIEW_RATE_NUMBER as review_rate_number,
     CALCULATED_HOST_LISTINGS_COUNT as calculated_host_listings_count,
     AVAILABILITY_365 as availability_365,
-    HOUSE_RULES as house_rules,
-    LICENSE as license
+    trim(HOUSE_RULES) as house_rules,
+    trim(LICENSE) as license
 
 from {{ source('airbnb', 'RAW_AIRBNB') }}
