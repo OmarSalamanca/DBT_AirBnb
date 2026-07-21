cfd4096948b0fc2e1271b40fe59792196d868eb9
Omar Mahmoud <ossossoss2345@gmail.com>
2026-07-21 10:29:58 +0000

    Refactor fact/model to depend on dimensions

--- models/marts/fact_listing.sql
+++ models/marts/fact_listing.sql
@@ -1,14 +1,20 @@
 select
 
-    listing_id,
-    host_id,
-    country_code,
-    price,
-    service_fee,
-    number_of_reviews,
-    reviews_per_month,
-    review_rate_number,
-    calculated_host_listings_count,
-    last_review
+    p.listing_id,
+    p.host_id,
+    p.country_code,
+    p.price,
+    p.service_fee,
+    p.number_of_reviews,
+    p.reviews_per_month,
+    p.review_rate_number,
+    p.calculated_host_listings_count,
+    p.last_review
 
-from {{ ref('stg_airbnb') }}
+from {{ ref('dim_property') }} p
+
+left join {{ ref('dim_host') }} h
+    on p.host_id = h.host_id
+
+left join {{ ref('dim_country') }} c
+    on p.country_code = c.country_code
